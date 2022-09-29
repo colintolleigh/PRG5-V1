e733bbd9916626516bd3a5390189ff1ea626413e
Colin Patty <89837160+colintolleigh@users.noreply.github.com>
2022-09-29 15:37:18 +0200

    Connection with the database and user log in and registration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>The Trooper Database</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
     <H1>The Official Trooper Database</H1>
     <p>
         Welcome to THE best database about Clone Troopers and Stormtroopers!
     </p>
+@endsection
 
-</body>
-</html>
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'show'])->name('welcome');
+
+
+Route::get('/about', function () {
+    return view('about');
 });

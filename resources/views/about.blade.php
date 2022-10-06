24803a63a05996f4c12d29f42f5febde0ce962cd
Colin Patty <89837160+colintolleigh@users.noreply.github.com>
2022-10-06 19:51:55 +0200

    Connection with the database and user log in and registration Extra database aangemaakt voor de troopers. Een log in en registration pagina. Een about us pagina aangemaakt.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>fkgvhlrvhlfhv</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
+<h1>
+    About Us
+</h1>
 
-</body>
-</html>
+    <p>
+        Hier vind je alles over ons.
+    </p>
+@endsection

28e9a9899660ce68155efd1749e19e9d2236bb5b
Colin Patty <89837160+colintolleigh@users.noreply.github.com>
2022-10-29 22:10:02 +0200

    Alle items wat gecentreerd op de schermen. Een profile pagina toegevoegd als een gebruiker is ingelogd. Die pagina ook aan de routes toegevoegd. De benoemde pagina's aangepast. Een nieuwe pagina toegevoegd, collection. De route ervan aangepast.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,6 +4,29 @@
     <div class="container">
         <h2>Profile</h2>
 
+        <p>Here you can edit your profile</p>
+
+        <form action="{{ url('/profile/') }}" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+            <input name="_method" type="hidden" value="PUT">
+            <input name="_token" type="hidden" value="{{csrf_token()}}">
+
+            <div class="row row-cols-1 row-cols-md-2 g-3 mb-4">
+                <div class="col">
+                    <div class="mb-3">
+                            <label for="name">Name</label>
+                            <span class="alert-danger"></span>
+                            <input id="name"
+                                   type="text"
+                                   name="name"
+                                   class="form-control"
+                                   value="{{old('name')}}"/>
+                            @error('name')
+                            <span>{{$message}}</span>
+                            @enderror
+                    </div>
+                </div>
+            </div>
+        </form>
 
     </div>
 
